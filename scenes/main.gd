beb6ddc9dc1fbcbb139eade08da898e5eb7b9aa2
Klein Metallicis <joaxz1@gmail.com>
2023-09-03 17:25:07 -0300

    Fixed timer

--- scenes/main.gd
+++ scenes/main.gd
@@ -22,5 +22,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Label.text=str(int($Timer.time_left))
+	$Label.text=str(snapped($Timer.time_left,0.01))
 	
