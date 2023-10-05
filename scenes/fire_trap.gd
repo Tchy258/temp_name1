08b654aa16a378a84f151e20ecba20f93af30ba0
nicolas.escobar <tchy258@yahoo.cl>
2023-10-05 11:45:32 -0300

    add: functional fire trap

--- scenes/fire_trap.gd
+++ scenes/fire_trap.gd
@@ -1,23 +1,29 @@
 extends AnimatableBody2D
-@onready var animation_player = $AnimationPlayer
-@export var player_id: int
-@onready var collision_detector = $Pivot/CollisionDetector
-@onready var spear_collision = $Pivot/CollisionDetector/SpearCollision
 
+@onready var area = $ClickableArea
+@onready var area_shape = $ClickableArea/CollisionShape2D
+@onready var animation_player = $AnimationPlayer
+@onready var collision_detector = $CollisionDetector
+@onready var far_end = $CollisionDetector/Square2
 var is_selected = false
 var reached_floor = false
 var speed = 0
+var player_id
 const GRAVITY = 9
-
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	collision_detector.connect("body_entered",_on_body_entered)
+	area_shape.disabled = true
 
-
+func _on_mouse_entered() -> void:
+	is_selected = true
+	
+func _on_mouse_exited() -> void:
+	is_selected = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
+func _process(delta):
 	if is_multiplayer_authority():
-		if Input.is_action_just_pressed("l_click") and is_selected:
+		if Input.is_action_just_pressed("l_click") and is_selected and reached_floor:
 			process_input.rpc()
 		if !reached_floor:
 			speed += min(GRAVITY * delta, 5)
@@ -26,26 +32,26 @@ func _process(delta: float):
 				var floor := colliders.get_collider() as TileMap
 				if floor:
 					reached_floor = true
-					connect("mouse_entered",_on_mouse_entered)
-					connect("mouse_exited",_on_mouse_exited)
-
-func _on_mouse_entered() -> void:
-	is_selected = true
-	
-func _on_mouse_exited() -> void:
-	is_selected = false
-
+					area.connect("mouse_entered",_on_mouse_entered)
+					area.connect("mouse_exited",_on_mouse_exited)
+					area_shape.disabled = false
+					
 
 func _on_body_entered(body: Node2D) -> void:
 	var colliding_body := body as CharacterBody2D
 	if colliding_body and colliding_body.has_method("bounce"):
-		colliding_body.bounce(spear_collision.global_position)
-
+		var distance_to_center = 17 if !far_end.disabled else 0
+		colliding_body.bounce(self.global_position - Vector2(0,distance_to_center))
 
 @rpc("call_local","reliable")
 func process_input() -> void:
 	if not animation_player.is_playing():
-			animation_player.play("activation")
+		animation_player.play("Activate")
+		animation_player.play("Keep")
+	elif animation_player.current_animation == "Keep":
+		animation_player.play("TurnOff")
+	else:
+		animation_player.play("TurnOff")
 
 func setup(player_data: Game.PlayerData):
 	set_multiplayer_authority(player_data.id)
@@ -58,3 +64,4 @@ func init(id:int, global_pos: Vector2):
 	set_multiplayer_authority(id,true)
 	player_id = id
 	global_position = global_pos
+
