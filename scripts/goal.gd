0f9201f688c851538d295dad2dc442fb2dff7ff5
David Parada <david.parada.munoz@gmail.com>
2023-11-22 19:26:10 -0300

    Update: Goal's sprite
    - Updated goal's sprite. Now it's a door.
    - Goal's body entered signal changed to one shot

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
+@onready var animation_player = $"../AnimationPlayer"
+
 func _ready():
-	body_entered.connect(_on_body_entered)
+	animation_player.play("Idle")
+	# 4 = CONNECT_ONE_SHOT
+	body_entered.connect(_on_body_entered, 4)
 	
 func _on_body_entered(_body: Node2D):
 	TimerManager.on_goal_reached.rpc()
+	animation_player.play("Open")
